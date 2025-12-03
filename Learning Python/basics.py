727f8cb8b06f60021f5bdd97fabaeb2f1c892051
shuklavg800-lgtm <shuklavg.800@gmail.com>
2025-12-04 01:32:56 +0530

    the data types and type conversion updated

--- Learning Python/basics.py
+++ Learning Python/basics.py
@@ -1,1 +1,44 @@
-print(53)
+# # print(53)
+# # # data types
+# # name ="prince"
+# # age= 20
+
+# # print(type(name))
+# # print(type(age))
+
+a=10
+b=20
+# print("before a ",a)
+# print("before b ",b)
+
+# # printting the sum diff etc
+# print("sum ",a+b)
+# print("diff ",a-b)
+# print("prod ",a*b)
+# print("quotient ",a/b) 
+# print("type of quotient : ",type(a/b))
+# # swapping of the variables without using the third variable
+# a=a+b
+# b=a-b
+# a=a-b
+# print("after a ",a)
+# print("after b ",b)
+
+# print("quotient ",a/b) 
+# print("type of quotient : ",type(a/b))
+# print((a==b))
+# print (not True)
+# print (not False)
+
+# name =input("enter ur name")
+# print(name,"welcome")
+
+val =input("enter some value ")
+print("the value is", val, "and the data type is ", type(val))
+# print(val+10) # this will result in TypeError: can only concatenate str (not "int") to str
+
+# for coverting the val to the int or float we use type casting
+print(type(int(val)))
+val= int(val)
+print("the after type casting value is", val, "and the data type is ", type(val))
+ 
