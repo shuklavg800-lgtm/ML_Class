cb976d1cc12b939a53b2d64ab0cb3f4be386e1ce
Prince Shukla <shuklavg800@gmail.com>
2026-01-27 21:33:51 +0530

    minor fixes

--- Learning Python/consonant_and_vowel.py
+++
@@ -1,6 +0,0 @@
-ch = input("Enter an alphabet: ").lower()
-
-if ch in 'aeiou':
-    print(ch, "is a vowel")
-else:
-    print(ch, "is a consonant")
--- Learning Python/product.py
+++
@@ -1,5 +0,0 @@
-a = float(input("Enter first number: "))
-b = float(input("Enter second number: "))
-product = a * b
-
-print("Product =", product)
